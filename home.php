6f2cc3d6dcfe7fcf69913c3d41f0ce594540f093
KRISHA2108 <gujaratikrisha6@gmail.com>
2025-04-04 12:17:04 +0530

    Php

---
+++ home.php
@@ -0,0 +1,22 @@
+<?php
+
+    echo "<h1> HELLO PHP!!👋</h1><br>";
+
+    echo " Sum of Two value<br><br>";
+
+    $a = 20;
+    $b = 30;
+
+    echo "Value of A : $a <br>";
+    echo "Value of B : $b <br><br>";
+
+    echo "--------------Addition--------------- <br><br>";
+
+    echo "Sum of A and B :". $a + $b . "<br><br>";
+    
+    echo "--------------Substrection--------------- <br><br>";
+
+    echo "Sub of A and B :". $a - $b;
+
+
+?>
